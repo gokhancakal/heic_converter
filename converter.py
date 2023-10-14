2e66a95a624e133c5e5c770aa6564e5521ca4843
Gökhan Çakal <gokhancakall@gmail.com>
2023-10-14 15:47:31 +0300

    Initial commit

--- converter.py
+++ converter.py
@@ -1,15 +1,14 @@
-import os
 from pillow_heif import register_heif_opener
 from PIL import Image
 
 register_heif_opener()
 
 
-def convert_heic_to_jpg(heic_file, output_directory):
+def convert_heic_to_jpg(heic_file, output):
     try:
         img = Image.open(heic_file)
-        jpg_file = os.path.join(output_directory, os.path.splitext(os.path.basename(heic_file))[0] + ".jpg")
-        img.save(jpg_file, "JPEG", quality=100)
+        img.save(output, "JPEG", quality=100)
         return True
     except Exception as e:
+        print(e)
         return False
